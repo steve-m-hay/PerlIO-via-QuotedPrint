0fa3190a3760b9b1e0a95233cc091a0f8863c471
Steve Hay <steve.m.hay@googlemail.com>
2017-12-22 12:54:54 +0000

    Add missing man page sections

--- lib/PerlIO/via/QuotedPrint.pm
+++ lib/PerlIO/via/QuotedPrint.pm
@@ -70,17 +70,13 @@ PerlIO::via::QuotedPrint - PerlIO layer for quoted-printable strings
 
 =head1 SYNOPSIS
 
- use PerlIO::via::QuotedPrint;
+    use PerlIO::via::QuotedPrint;
 
- open( my $in, '<:via(QuotedPrint)', 'file.qp' )
-   or die "Can't open file.qp for reading: $!\n";
+    open(my $in, '<:via(QuotedPrint)', 'file.qp') or
+        die "Can't open file.qp for reading: $!\n";
 
- open( my $out, '>:via(QuotedPrint)', 'file.qp' )
-   or die "Can't open file.qp for writing: $!\n";
-
-=head1 VERSION
-
-This documentation describes version 0.09.
+    open(my $out, '>:via(QuotedPrint)', 'file.qp') or
+        die "Can't open file.qp for writing: $!\n";
 
 =head1 DESCRIPTION
 
@@ -88,19 +84,72 @@ This module implements a PerlIO layer that works on files encoded in the
 quoted-printable format.  It will decode from quoted-printable while reading
 from a handle, and it will encode as quoted-printable while writing to a handle.
 
-=head1 REQUIRED MODULES
+=head1 EXPORTS
 
- MIME::QuotedPrint (any)
+I<None>.
+
+=head1 KNOWN BUGS
+
+I<None>.
+
+=head1 FEEDBACK
+
+Patches, bug reports, suggestions or any other feedback is welcome.
+
+Patches can be sent as GitHub pull requests at
+L<https://github.com/steve-m-hay/PerlIO-via-QuotedPrint/pulls>.
+
+Bug reports and suggestions can be made on the CPAN Request Tracker at
+L<https://rt.cpan.org/Public/Bug/Report.html?Queue=PerlIO-via-QuotedPrint>.
+
+Currently active requests on the CPAN Request Tracker can be viewed at
+L<https://rt.cpan.org/Public/Dist/Display.html?Status=Active;Queue=PerlIO-via-QuotedPrint>.
+
+Please test this distribution.  See CPAN Testers Reports at
+L<http://www.cpantesters.org/> for details of how to get involved.
+
+Previous test results on CPAN Testers Reports can be viewed at
+L<http://www.cpantesters.org/distro/P/PerlIO-via-QuotedPrint.html>.
+
+Please rate this distribution on CPAN Ratings at
+L<http://cpanratings.perl.org/rate/?distribution=PerlIO-via-QuotedPrint>.
 
 =head1 SEE ALSO
 
-L<PerlIO::via>, L<MIME::QuotedPrint>, L<PerlIO::via::Base64>,
-L<PerlIO::via::MD5>, L<PerlIO::via::StripHTML>, L<PerlIO::via::Rotate>.
+L<PerlIO::via>,
+L<MIME::QuotedPrint>.
 
 =head1 ACKNOWLEDGEMENTS
 
-Based on example that was initially added to MIME::QuotedPrint.pm for the
-5.8.0 distribution of Perl.
+Based on an example in the standard library module MIME::QuotedPrint in Perl
+(version 5.8.0).
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from CPAN (see
+L<perlmodlib/"CPAN"> for details) at
+
+L<https://metacpan.org/release/PerlIO-via-QuotedPrint> or
+
+L<http://search.cpan.org/dist/PerlIO-via-QuotedPrint/> or
+
+L<http://www.cpan.org/authors/id/S/SH/SHAY/> or
+
+L<http://www.cpan.org/modules/by-module/PerlIO/>.
+
+The latest source code is available from GitHub at
+L<https://github.com/steve-m-hay/PerlIO-via-QuotedPrint>.
+
+=head1 INSTALLATION
+
+See the F<INSTALL> file.
+
+=head1 AUTHOR
+
+Elizabeth Mattijsen E<lt>L<liz@dijkmat.nl|mailto:liz@dijkmat.nl>E<gt>.
+
+Steve Hay E<lt>L<shay@cpan.org|mailto:shay@cpan.org>E<gt> is now maintaining
+PerlIO::via::QuotedPrint as of version 0.08.
 
 =head1 COPYRIGHT
 
@@ -113,5 +162,17 @@ Copyright (C) 2015 Steve Hay.  All rights reserved.
 This module is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself, i.e. under the terms of either the GNU General
 Public License or the Artistic License, as specified in the F<LICENCE> file.
+
+=head1 VERSION
+
+Version 0.09
+
+=head1 DATE
+
+TODO
+
+=head1 HISTORY
+
+See the F<Changes> file.
 
 =cut
