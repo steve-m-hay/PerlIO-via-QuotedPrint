dbb0c294bc1298e561513ef859da6b0756476cf0
Steve Hay <steve.m.hay@googlemail.com>
2022-05-22 11:52:29 +0100

    Bump version for next release

--- lib/PerlIO/via/QuotedPrint.pm
+++ lib/PerlIO/via/QuotedPrint.pm
@@ -12,7 +12,7 @@ use 5.008001;
 # be as strict as possible
 use strict;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 # modules that we need
 use MIME::QuotedPrint (); # no need to pollute this namespace
@@ -163,11 +163,11 @@ Public License or the Artistic License, as specified in the F<LICENCE> file.
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =head1 DATE
 
-22 May 2022
+TODO
 
 =head1 HISTORY
 
